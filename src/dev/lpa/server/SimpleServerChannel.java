702a6455b02e863b44e02fbde6354c61d484c7ea
Gheorghi <valentina161012@gmail.com>
2026-02-17 19:59:06 +0200

    NetworkPolling(fail)

--- src/dev/lpa/server/SimpleServerChannel.java
+++ src/dev/lpa/server/SimpleServerChannel.java
@@ -7,30 +7,44 @@ import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SimpleServerChannel {
     static void main() {
 
         try (ServerSocketChannel serverChannel = ServerSocketChannel.open()) {
             serverChannel.socket().bind(new InetSocketAddress(5000));
+            serverChannel.configureBlocking(false);
             System.out.println("Server listening on port " +
                     serverChannel.socket().getLocalPort());
-            while (true) {
-                SocketChannel clientChannel = serverChannel.accept();
-                System.out.printf("Client %s connected\n", clientChannel.socket().getRemoteSocketAddress());
-                ByteBuffer buffer = ByteBuffer.allocate(1024);
-                SocketChannel channel = clientChannel;
-                int readBytes = clientChannel.read(buffer);
+            List<SocketChannel> clientChannels = new ArrayList<>();
 
-                if(readBytes > 0 ) {
-                    buffer.flip();
-                    channel.write(ByteBuffer.wrap("Echo from server: ".getBytes()));
-                    while(buffer.hasRemaining()) {
-                        channel.write(buffer);
-                    }
+            while (true) {
+//                System.out.println("Waiting for connection...");
+                SocketChannel clientChannel = serverChannel.accept();
+                if (clientChannel != null) {
+                    clientChannels.add(clientChannel);
+                    System.out.printf("Client %s connected\n", clientChannel.socket().getRemoteSocketAddress());
+                    clientChannel.configureBlocking(false);
                 }
-                else if(readBytes == -1) {
-                    System.out.printf("Connection to %s lost", channel.socket().getRemoteSocketAddress() );
+
+                ByteBuffer buffer = ByteBuffer.allocate(1024);
+                for (int i = 0; i < clientChannels.size(); i++) {
+                    SocketChannel channel = clientChannels.get(i);
+                    clientChannels.add(clientChannel);
+                    int readBytes = clientChannel.read(buffer);
+
+                    if (readBytes > 0) {
+                        buffer.flip();
+                        channel.write(ByteBuffer.wrap("Echo from server: ".getBytes()));
+                        while (buffer.hasRemaining()) {
+                            channel.write(buffer);
+                        }
+                    } else if (readBytes == -1) {
+                        System.out.printf("Connection to %s lost", channel.socket().getRemoteSocketAddress());
+                        clientChannels.remove(i);
+                    }
                 }
             }
         } catch (IOException e) {
